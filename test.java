c9ddb97062db99dbcf5367b838db5942e148ca18
alin4627 <alin4627@gmail.com>
2018-12-01 16:29:39 -0500

    the csv data & java convert to string

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class test {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/data.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.print(scanner.next()+"|");
+        }
+        scanner.close();
+    }
+
+}
